d1ca0b30377af418efe3ba342e38a665740d6c7b
Justin Svegliato <justin.svegliato1@marist.edu>
2014-04-17 09:33:51 -0400

    Implemented the semantic analyzer and added helper methods to the symbol table

--- public_html/assets/js/system/compiler/symbol-table.js
+++ public_html/assets/js/system/compiler/symbol-table.js
@@ -6,9 +6,10 @@
 function SymbolTable() {
     this.scopeLevelId = 0;
     this.tree = new TreeModel();
-    
+
     // Sets the current scope to 0
     this.currentScope = this.tree.parse(new Scope(this.scopeLevelId));
+    this.root = this.currentScope;
 };
 
 // Adds a symbol to the symbol table given a type, variable, and line number.
@@ -16,26 +17,30 @@ SymbolTable.prototype.addSymbol = function(type, variable, lineDeclared) {
     // Only add the symbol to the table if it does not yet exist
     if (!(variable in this.currentScope.model.variables)) {
         // Displays the symbol
-        SymbolTableDisplay.addSymbol(this.currentScope.model.level, lineDeclared, variable, type);
-        
+        SymbolTableDisplay.addSymbol(this.currentScope.model.level - 1, lineDeclared, variable, type);
+        TreeDisplay.addSymbol(this.currentScope, variable, type);
+
         // Creates the symbol and adds it to the current scope
-        var symbol = new Symbol(type, variable, 0);
+        var symbol = new Symbol(type, variable, lineDeclared);
         this.currentScope.model.variables[variable] = symbol;
         return symbol;
-    }           
+    }
     return false;
 };
 
 // Enters a new scope (this is denoted by '{')
 SymbolTable.prototype.enterScope = function() {
     // Create a new scope
-    var scope = this.tree.parse(new Scope(this.scopeLevelId++));
-    
+    var scope = this.tree.parse(new Scope(++this.scopeLevelId));
+
     // Make this scope a child of the current scope
     this.currentScope.addChild(scope);
     
+    // Registers the new scope in the tree display    
+    TreeDisplay.addScope(scope);
+
     // Set this new scope as the current scope
-    this.currentScope = scope;
+    this.currentScope = scope;  
 };
 
 // Exists the current scope (this is denoted by '}')
@@ -44,11 +49,52 @@ SymbolTable.prototype.exitScope = function() {
     this.currentScope = this.currentScope.parent;
 };
 
+// Checks if the specified identifier is in the current scope
+SymbolTable.prototype.checkScope = function(identifier) {
+    return this.getVariableData(identifier, this.currentScope);
+};
+
+// Gets the scope at the specified level
+SymbolTable.prototype.getScope = function(level) {
+    return this.currentScope.first(function(node) {
+        return node.model.level === level;
+    });
+};
+
+// Checks if every variable has been initialized or used (it outputs an error otherwise)
+SymbolTable.prototype.check = function() {
+    this.currentScope.walk(function (node) {
+        for (var key in node.model.variables) {
+            var variable = node.model.variables[key];
+            if (!variable.isUsed) {
+                LogDisplay.logSemanticAnalyzerWarningResult(variable.lineNumber, "Variable is not used", variable.variable);
+            } else if (variable.isUsed && !variable.isInitilized) {
+                LogDisplay.logSemanticAnalyzerWarningResult(variable.lineNumber, "Variable is not initialized", variable.variable);
+            }
+        }
+    });
+};
+
+// Retrieves the data associated with the specified identifier at this given scope
+// or perhaps the ancestors of the specified scope
+SymbolTable.prototype.getVariableData = function(identifier, scope) {    
+    if (identifier in scope.model.variables) {
+        return scope.model.variables[identifier];
+    } else {
+        if (!("parent" in scope)) {
+            return false;
+        }
+        return this.getVariableData(identifier, scope.parent);
+    }       
+};
+
 // Creates a class containing all information for symbols
-function Symbol(type, variable, value) {
+function Symbol(type, variable, lineNumber) {
     this.type = type;
     this.variable = variable;
-    this.value = value;    
+    this.lineNumber = lineNumber;
+    this.isUsed = false;
+    this.isInitilized = false;
 };
 
 // Creates a class containing all information for scopes
